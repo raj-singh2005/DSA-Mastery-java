ab7026fd1f4b314fceb342f54738f91ce930dc3a
Raj singh <raj.singh2005.dev@gmail.com>
2026-04-05 13:29:50 +0530

    04-05-2026 0242-valid-anagram Easy .java 4 ms (82.17%) 44.5 MB (70%)

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] arr = new int[26] ;
+        int[] count = new int[26] ;
         int sLen = s.length() ;
         int tLen = t.length() ;
-      if(sLen != tLen){
-        return false ;
-      }
-      for(int i = 0 ; i < sLen ; i++){
-        arr[s.charAt(i) - 'a']++ ;
-        arr[t.charAt(i) - 'a']-- ;
-      }
-      for(int num : arr){
-        if(num != 0){
+
+        if(sLen != tLen){
             return false ;
         }
-      }
+        for(int i = 0 ; i < sLen ; i++){
+            count[s.charAt(i) - 'a']++ ;
+            count[t.charAt(i) - 'a']-- ;
+        }
 
-      return true ;
+        for(int val : count){
+            if(val != 0){
+                return false ;
+            }
+        }
+
+        return true ;
     }
 }
