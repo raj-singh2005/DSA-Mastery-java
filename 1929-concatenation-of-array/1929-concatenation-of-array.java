d5d9212ba06895f4823dfa9949b68c39194ad80b
Raj singh <raj.singh2005.dev@gmail.com>
2026-04-05 13:05:38 +0530

    04-05-2026 1929-concatenation-of-array Easy .java 1 ms (96.46%) 47.1 MB (67.27%)

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-    int n = nums.length  ;
-    int[] ans = new int[2*n];
-    for(int i = 0 ; i < n ; i++){
-        ans[i] = nums[i] ;
-        ans[n + i ] = nums[i] ;
-    } 
+        int n = nums.length ;
+        int[] arr = new int[2*n] ;
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = nums[i] ;
+            arr[ i + n ] = nums[i] ;
+        }
 
-    return ans ;
+        return arr ;
     }
 }
