8cca3b6f81acc7e0dcc59cc7f9e6178b3513dbf6
Raj singh <raj.singh2005.dev@gmail.com>
2026-07-25 01:44:28 +0530

    07-25-2026 0026-remove-duplicates-from-sorted-array Easy .java 1 ms (78.02%) 46.9 MB (9.37%)

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,9 +3,12 @@ class Solution {
         int k = 1 ;
         int i = 0 ;
         int j = i+1 ;
+        if(nums.length == 0){
+        return -1 ;
+        }
 
         while(j < nums.length){
-            if(nums[j] == nums[j-1]){
+            if(nums[j] == nums[i]){
                 j++ ;
                 continue ;
             }
