9ca92ae088b73b5da50ca377f558300cfde6a447
Raj singh <raj.singh2005.dev@gmail.com>
2026-07-25 01:11:06 +0530

    07-25-2026 0001-two-sum Easy .java 2 ms (99.29%) 47.3 MB (23.34%)

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,9 +2,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> map = new HashMap<>() ;
         int n = nums.length ;
-        for(int i = 0 ; i < n ; i++){
-            map.put(nums[i],i) ;
-        }
+       
 
         for(int i = 0 ; i < n ; i++){
             int diff = target - nums[i] ;
@@ -12,6 +10,8 @@ class Solution {
             if(map.containsKey(diff) && map.get(diff) != i ){
                 return new int[]{i,map.get(diff)} ;
             }
+
+            map.put(nums[i],i) ;
 
         }
         return new int[]{-1,-1} ;
